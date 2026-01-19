cdb0328b5ade169c061b82f19034b333b9c21121
Mauro Lacy <mauro@lacy.com.es>
2026-01-19 14:02:32 +0100

    Read paths from file arg

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,16 @@
 
 mod bfs;
 
-use std::{fs::File, io::{self, BufRead, BufReader}};
+use std::{env::args, fs::File, io::{self, BufRead, BufReader}};
 
 use bfs::GraphChallenge;
 
 fn main() {
-    // Read all lines from test1.txt
-    let file = File::open("test1.txt").unwrap();
+    // First argument is the file to read
+    let file_path = args().nth(1).unwrap();
+    let file = File::open(file_path).unwrap();
     let reader = BufReader::new(file);
     let lines = reader.lines().collect::<Result<Vec<String>, io::Error>>().unwrap();
-    //let lines = io::stdin().lines().collect::<Result<Vec<String>, io::Error>>().unwrap();
 
     // Call the GraphChallenge function
     let result = GraphChallenge(lines.iter().map(|line| line.as_str()).collect());
